69f4c51a868ddafc1cdf20248611273dda4f1087
Анастасия Киреева <nastusha00188@gmail.com>
2022-12-19 19:50:47 +0300

    Bonus

---
+++ src/Java_Miles.java
@@ -0,0 +1,10 @@
+public class Java_Miles {
+
+    public static void main(String[] args) {
+
+        int Mile = 20;
+        int TicketPrice = 100;
+
+        System.out.println((TicketPrice / Mile) + " Бонусов начислено");
+    }
+    }
