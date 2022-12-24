6a9891d7f24f5945dda4d966a27fbccdafc92d50
Анастасия Киреева <nastusha00188@gmail.com>
2022-12-24 19:59:33 +0300

    Title

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int mile = 20;
+        int ticketPrice = 100;
+
+        System.out.println((ticketPrice / mile) + " Бонусов начислено");
+    }
+}
